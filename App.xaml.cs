44e179ccbc904296daab2d15e820e109da5e5469
Rianzinhu <rianrochaaraujo1@gmail.com>
2025-04-11 19:32:01 -0300

    CoinFlip

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Coinflip
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Coinflip
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
